f87c24f4b7b977ea07050a34c45d76559c5fe144
algamor59 <algamor59@gmail.com>
2025-02-07 12:24:35 +0100

    Fix tests

--- test/Article.test.js
+++ test/Article.test.js
@@ -1,7 +1,17 @@
-const Article = require("../src/Article");
-const { addQuantity } = require("../src/Article");
-const { consultQuantity} = require("../src/Article")
-const { removeQuantity} = require("../src/Article")
+const { addQuantity } = require("../src/Warehouse");
+const { consultQuantity} = require("../src/Warehouse")
+const { removeQuantity} = require("../src/Warehouse")
+const {consultFlag}= require("../src/Warehouse")
+
+describe("Gestion des stocks", () => {
+    
+    beforeEach(() => {
+        // Réinitialisation du stock avant chaque test pour être plus à l'aise dans mes tests et manipulations de quantités
+        stock["Clavier"] = 0;
+        stock["Écran"] = 0;
+        stock["Casque"] = 0;
+        stock["Clé USB"] = 0;
+    });
 
 describe("Ajout de quantité à un article", () => {
     test("Ajout valide : la quantité doit être mise à jour", () => {
@@ -11,24 +21,24 @@ describe("Ajout de quantité à un article", () => {
     });
 
     test("Ajout à un article inexistant : doit générer une erreur", () => {
-        expect(() => addQuantity("Souris", 5)).toThrow("L'article Souris n'existe pas !");
+        expect(() => addQuantity("Casque", 5)).toThrow("Invalid article");
     });
 
     test("Ajout d'une quantité négative : doit générer une erreur", () => {
-        expect(() => addQuantity("Clavier", -5)).toThrow("La quantité doit être un entier positif !");
+        expect(() => addQuantity("Clavier", -5)).toThrow("Quantity must be positive");
     });
 
-    test("Ajout d'une quantité non entière (nombre à virgule) : doit générer une erreur", () => {
-        expect(() => addQuantity("Clavier", 3.5)).toThrow("La quantité doit être un entier positif !");
+    test("Ajout d'une quantité float : doit générer une erreur", () => {
+        expect(() => addQuantity("Clavier", 3.5)).toThrow("Quantity must be positive");
     });
 
     test("Ajout d'une quantité non valide (texte) : doit générer une erreur", () => {
-        expect(() => addQuantity("Clavier", "dix")).toThrow("La quantité doit être un entier positif !");
+        expect(() => addQuantity("Clavier", "dix")).toThrow("Quantity must be positive");
     });
 
     test("Ajout d'une quantité null ou undefined : doit générer une erreur", () => {
-        expect(() => addQuantity("Clavier", null)).toThrow("La quantité doit être un entier positif !");
-        expect(() => addQuantity("Clavier", undefined)).toThrow("La quantité doit être un entier positif !");
+        expect(() => addQuantity("Clavier", null)).toThrow("Quantity must be positive");
+        expect(() => addQuantity("Clavier", undefined)).toThrow("Quantity must be positive");
     });
 });
 
@@ -39,41 +49,62 @@ describe("Consultation de la quantité d'un article", () => {
     });
 
     test("Article inexistant : doit générer une erreur", () => {
-        expect(() => consultQuantity("Tablette")).toThrow("L'article Tablette n'existe pas !");
+        expect(() => consultQuantity("Tablette")).toThrow("Invalid article");
     });
 });
 
 describe("Retrait de quantité d'un article", () => {
     test("Retrait valide : la quantité doit être mise à jour", () => {
+        //Je dois ajouter de la quantité pour éviter d'être bloqué par la fonctione si je retire du stock dans le négatif
         addQuantity("Casque", 12);
+        const previousStock = consultQuantity("Clavier");
         removeQuantity("Casque", 5);
-        expect(consultQuantity("Casque")).toBe(7);
+        expect(consultQuantity("Casque")).toBe(previousStock-5);
     });
 
     test("Retrait d'un article inexistant : doit générer une erreur", () => {
-        expect(() => removeQuantity("Imprimante", 3)).toThrow("L'article Imprimante n'existe pas !");
+        expect(() => removeQuantity("Imprimante", 3)).toThrow("Invalid article");
     });
 
     test("Retrait d'une quantité supérieure au stock disponible : doit générer une erreur", () => {
-        addQuantity("Clé USB", 4);
-        expect(() => removeQuantity("Clé USB", 10)).toThrow("Stock insuffisant pour l'article Clé USB !");
+        expect(() => removeQuantity("Clé USB", 100)).toThrow("Insufisant stock");
     });
 
     test("Retrait d'une quantité négative : doit générer une erreur", () => {
-        expect(() => removeQuantity("Clé USB", -2)).toThrow("La quantité doit être un entier positif !");
+        expect(() => removeQuantity("Clé USB", -2)).toThrow("Quantity must be positive");
     });
 
     test("Retrait d'une quantité non entière (nombre à virgule) : doit générer une erreur", () => {
-        expect(() => removeQuantity("Clé USB", 2.5)).toThrow("La quantité doit être un entier positif !");
+        expect(() => removeQuantity("Clé USB", 2.5)).toThrow("Quantity must be positive");
     });
 
     test("Retrait d'une quantité non valide (texte) : doit générer une erreur", () => {
-        expect(() => removeQuantity("Clé USB", "cinq")).toThrow("La quantité doit être un entier positif !");
+        expect(() => removeQuantity("Clé USB", "cinq")).toThrow("Quantity must be positive");
     });
 
     test("Retrait d'une quantité null ou undefined : doit générer une erreur", () => {
-        expect(() => removeQuantity("Clé USB", null)).toThrow("La quantité doit être un entier positif !");
-        expect(() => removeQuantity("Clé USB", undefined)).toThrow("La quantité doit être un entier positif !");
+        expect(() => removeQuantity("Clé USB", null)).toThrow("Quantity must be positive");
+        expect(() => removeQuantity("Clé USB", undefined)).toThrow("Quantity must be positive");
+    });
+});
+
+describe("Consultation du rapport de tous les articles", () => {
+    test("Affichage de tous les stocks", () => {
+        addQuantity("Écran", 7);
+        addQuantity("Clé USB", 10);
+        addQuantity("Souris", 11);
+        expect(consultQuantity("Écran")).toBe(7);
+        expect(consultQuantity("Clé USB")).toBe(10);
+        expect(consultQuantity("Souris")).toBe(11);
+        expect(consultQuantity("Clavier")).toBe(0);
+        expect(consultFlagLowQuantity("Écran").toBe(true));
+        expect(consultFlagOutOfStock("Écran").toBe(false));
+        expect(consultFlagLowQuantity("Clé USB").toBe(true));
+        expect(consultFlagOutOfStock("Clé USB").toBe(false));
+        expect(consultFlagLowQuantity("Souris").toBe(false));
+        expect(consultFlagOutOfStock("Souris").toBe(false));
+        expect(consultFlagLowQuantity("Clavier").toBe(false));
+        expect(consultFlagOutOfStock("Clavier").toBe(true));
     });
 });
 });
